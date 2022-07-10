f77fe2f0ddaa0d028f83a8f80f27e1446f9624ba
Gustavo Hidalgo Xavier <guhidalgox@gmail.com>
2022-07-10 15:27:53 -0300

    first commit

---
+++ src/test/java/com/gustavohxavier/contractapi/ContractApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavohxavier.contractapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
